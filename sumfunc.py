379d8e18db1835aa089c1ddc5126f8f43adb2aee
Your Name <eromoselejonthan@gmail.com>
2022-09-28 19:54:16 +0100

    modified

--- sumfunc.py
+++ sumfunc.py
@@ -5,13 +5,13 @@ from typing import List
 from unittest import result
 
 
-# def sumsol(h):
-#     sum = 0
-#     for i in h :
-#         sum =sum+ i 
-#     return sum
-# k =(2,4,3,6,8)
-# print(sumsol(k))
+def sumsol(h):
+    sum = 0
+    for i in h :
+        sum =sum+ i 
+    return sum
+k =(2,4,3,6,8)
+print(sumsol(k))
 
 
 def runningsum(nums:List[int])->List[int]:
@@ -24,47 +24,46 @@ def runningsum(nums:List[int])->List[int]:
 k = (4,6,7)
 print(runningsum(k))
 
-# def twosums(nums,target):
-#     fin =[]
+def twosums(nums,target):
+    fin =[]
 
-#     for num in range(len(nums)):
-#         for i in range(num+1,len(nums)):
-#             sum = nums[num] + nums[i]
-#             if sum == target:
-#                 fin.append(index(num))
-#                 fin.append(index(i))
-#                 return fin
+    for num in range(len(nums)):
+        for i in range(num+1,len(nums)):
+            sum = nums[num] + nums[i]
+            if sum == target:
+                fin.append(index(num))
+                fin.append(index(i))
+                return fin
                 
 
-# p =5
 
-# print(twosums(k,p))
+print(twosums(k,p))
 
-# def palindrome(x):
-#     x = str(x)
-#     reversedx = x[::-1] 
-#     if x == reversedx:
-#         return True
-#     else:
-#         return False
+def palindrome(x):
+    x = str(x)
+    reversedx = x[::-1] 
+    if x == reversedx:
+        return True
+    else:
+        return False
 
-# print(palindrome(102))
-
+print(palindrome(102))
 
 
-# def longest_subString(x):
-#     charset = set()
-#     lent = 0
-#     result=0
-#     for i in range(len(x)):
-#         while x[i] in charset:
-#             charset.remove(x[i])
-#             lent+=1
-#         charset.add(x[i])
-#         result = max (result,i-lent+1)
-#     return result ,i,lent
+
+def longest_subString(x):
+    charset = set()
+    lent = 0
+    result=0
+    for i in range(len(x)):
+        while x[i] in charset:
+            charset.remove(x[i])
+            lent+=1
+        charset.add(x[i])
+        result = max (result,i-lent+1)
+    return result ,i,lent
 
         
-# print(longest_subString("pwwkew"))
+
 
     
